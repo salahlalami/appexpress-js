38023edf6c70a13059d71b5af1d27e898b00792d
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-01-30 03:17:40 +0100

    final pug app

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require("./handlers/passport")(passport); // pass passport for configuration
 const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views")); // this is the folder where we keep our pug files
-app.set("view engine", "ejs"); // we use the engine pug, mustache or EJS work great too
+app.set("view engine", "pug"); // we use the engine pug, mustache or EJS work great too
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, "public")));

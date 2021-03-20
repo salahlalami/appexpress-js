b3adda7f703d8d4136351e4d66d2abbdcc35dbdf
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-03-20 20:42:34 +0100

    update mongo connect

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-let MongoStore = require("connect-mongo").default;
+const MongoStore = require("connect-mongo");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");

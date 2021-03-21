a81fb81431380bf8b963bb2a8b583ed2022d1559
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-03-21 10:35:07 +0100

    add x-auth-token to header

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Expose-Headers", "Content-Length");
   res.header(
     "Access-Control-Allow-Headers",
-    "Accept, Authorization, Content-Type, X-Requested-With, Range"
+    "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
   );
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);

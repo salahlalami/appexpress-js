d6814906ff6cc93de41d56b638310bfaa0e12d18
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-05-11 04:58:46 +0100

    update create invoice

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -5,86 +5,116 @@ const custom = require("./helpersControllers/custom");
 const crudController = require("./helpersControllers/crudController");
 const methods = crudController.createCRUDController("Invoice");
 
-delete methods["create"];
+// delete methods["create"];
 delete methods["update"];
+// try {
+//   // Creating a new document in the collection
+//   const result = await new Model(req.body).save();
 
-methods.create = async (req, res) => {
-  try {
-    var { items = [], taxRate = 0, discount = 0 } = req.body;
+//   // Returning successfull response
+//   return res.status(200).json({
+//     success: true,
+//     result,
+//     message: "Successfully Created the document in Model ",
+//   });
+// } catch (err) {
+//   // If err is thrown by Mongoose due to required validations
+//   if (err.name == "ValidationError") {
+//     return res.status(400).json({
+//       success: false,
+//       result: null,
+//       message: "Required fields are not supplied",
+//     });
+//   } else {
+//     // Server Error
+//     return res.status(500).json({
+//       success: false,
+//       result: null,
+//       message: "Oops there is an Error",
+//     });
+//   }
+// }
+// methods.create = async (req, res) => {
+//   try {
+//     var { items = [], taxRate = 0, discount = 0 } = req.body;
 
-    // default
-    var subTotal = 0;
-    var taxTotal = 0;
-    var total = 0;
-    var credit = 0;
+//     // default
+//     var subTotal = 0;
+//     var taxTotal = 0;
+//     var total = 0;
+//     var credit = 0;
 
-    //Calculate the items array with subTotal, total, taxTotal
-    items = items.map((item) => {
-      let total = item["quantity"] * item["price"];
-      //sub total
-      subTotal += total;
-      //item total
-      item["total"] = total;
-      return item;
-    });
+//     //Calculate the items array with subTotal, total, taxTotal
+//     items.map((item) => {
+//       let total = item["quantity"] * item["price"];
+//       //sub total
+//       subTotal += total;
+//       //item total
+//       item["total"] = total;
+//     });
 
-    taxTotal = subTotal * taxRate;
-    total = subTotal + taxTotal;
+//     taxTotal = subTotal * taxRate;
+//     total = subTotal + taxTotal;
 
-    let body = req.body;
+//     let body = req.body;
 
-    body["subTotal"] = subTotal;
-    body["taxTotal"] = taxTotal;
-    body["total"] = total;
-    body["credit"] = credit;
+//     body["subTotal"] = subTotal;
+//     body["taxTotal"] = taxTotal;
+//     body["total"] = total;
+//     body["credit"] = credit;
 
-    // Creating a new document in the collection
-    var save = await new Model(body).save();
+//     // Creating a new document in the collection
+//     let save = await new Model(body).save();
 
-    let modelId = save.id;
+//     // let modelId = save.id;
 
-    //Calculate credited amount
-    const findById = await Model.findById(modelId).populate("clientPayment");
-    if (findById["clientPayment"].length > 0) {
-      findById["clientPayment"].map((payment) => {
-        credit += payment.amount;
-      });
-    }
+//     // //Calculate credited amount
+//     // const findById = await Model.findById(modelId).populate("clientPayment");
+//     // if (findById["clientPayment"].length > 0) {
+//     //   findById["clientPayment"].map((payment) => {
+//     //     credit += payment.amount;
+//     //   });
+//     // }
 
-    let update = {};
-    update["credit"] = credit;
+//     // let update = {};
+//     // update["credit"] = credit;
 
-    // //Calculate payment status
-    if (total - discount - credit <= 0) {
-      update["paymentStatus"] = "paid";
-    }
+//     // // //Calculate payment status
+//     // if (total - discount - credit <= 0) {
+//     //   update["paymentStatus"] = "paid";
+//     // }
 
-    // // Find document by id and updates with the required fields
-    const result = await Model.findOneAndUpdate({ _id: modelId }, update, {
-      new: true,
-    })
-      .populate("client")
-      .exec();
-  } catch (err) {
-    console.log(err);
+//     // // // Find document by id and updates with the required fields
+//     // const result = await Model.findOneAndUpdate({ _id: modelId }, update, {
+//     //   new: true,
+//     // })
+//     //   .populate("client")
+//     //   .exec();
+//     res.status(200).json({
+//       success: true,
+//       result: save,
+//       message: "Successfully Created the document in Model ",
+//     });
+//   } catch (err) {
+//     console.log(err);
 
-    // If err is thrown by Mongoose due to required validations
-    if (err.name == "ValidationError") {
-      return res.status(400).json({
-        success: false,
-        result: null,
-        message: "Required fields are not supplied",
-      });
-    } else {
-      // Server Error
-      return res.status(500).json({
-        success: false,
-        result: null,
-        message: "Oops there is an Error",
-      });
-    }
-  }
-};
+//     // If err is thrown by Mongoose due to required validations
+//     if (err.name == "ValidationError") {
+//       return res.status(400).json({
+//         success: false,
+//         result: null,
+//         message: "Required fields are not supplied",
+//       });
+//     } else {
+//       // Server Error
+//       return res.status(500).json({
+//         success: false,
+//         result: null,
+//         message: "Oops there is an Error",
+//       });
+//     }
+//   }
+// };
 
 methods.update = async (req, res) => {
   try {
